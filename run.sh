0f6604729c0166f34b5349908b07f50f9f3ee3fd
gelbal <firat.gelbal@gmail.com>
2020-05-08 19:04:52 +0300

    WIP changes to make it work on balenaOS

--- run.sh
+++ run.sh
@@ -3,12 +3,17 @@
 OS_VERSION=$(echo "$BALENA_HOST_OS_VERSION" | cut -d " " -f 2)
 echo "OS Version is $OS_VERSION"
 
-mod_dir="example_module_${BALENA_DEVICE_TYPE}_${OS_VERSION}*"
+modprobe i2c-dev
+
+mod_dir="seeed-voicecard_${BALENA_DEVICE_TYPE}_${OS_VERSION}*"
 for each in $mod_dir; do
 	echo Loading module from "$each"
-	insmod "$each/hello.ko"
-	lsmod | grep hello
-	rmmod hello
+	cp "$each/snd-soc-ac108.ko"
+	insmod "$each/snd-soc-ac108.ko"
+	insmod "$each/snd-soc-wm8960.ko"
+	insmod "$each/snd-soc-seeed-voicecard.ko"
+	lsmod | grep snd-soc-seeed-voicecard
+	#rmmod snd-soc-seeed-voicecard
 done
 
 while true; do
